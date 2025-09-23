0cf2861aa141cb6f6e4a4558d454ee2523d04215
Artem Prokofiev <artem-prokofiev@yandex.ru>
2025-09-23 23:21:49 +0300

    домашка 4

---
+++ netkliks-app/src/app/constants/films.constants.ts
@@ -0,0 +1,33 @@
+import { Film } from "../models/film.model";
+
+export const FILMS: Film[] = [
+  {
+    id: 0,
+    title: 'Марсианин 1',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: '/netkliks_03/film.html',
+    image: './images/film.png',
+  },
+
+  {
+    id: 1,
+    title: 'Марсианин 2',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: '/netkliks_03/film.html',
+    image: './images/film.png',
+  },
+
+  {
+    id: 2,
+    title: 'Марсианин 3',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: '/netkliks_03/film.html',
+    image: './images/film.png',
+  },
+];
